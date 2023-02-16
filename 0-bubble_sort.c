cb1913173c6d1947d7e33a99a751a2ce944cc44d
Idowu-David <idowudavidodun@gmail.com>
2023-02-16 14:43:37 +0000

    add: bubnle sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using the Bubble sort algorithm
+ * @array: pointer to the unsorted array
+ * @size: size of the array
+ * Return: none(void)
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	for(i = 0; i < size; i++)
+	{
+		swapped = 0;
+		for(j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+			else
+				continue;
+		if (swapped == 0)
+			return;
+		}
+	}
+}
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
