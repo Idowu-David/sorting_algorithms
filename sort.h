8a66a772f36c23ea3670be0230ffd735ff10c0f1
Idowu-David <idowudavidodun@gmail.com>
2023-02-16 09:52:29 +0000

    add: header files

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
